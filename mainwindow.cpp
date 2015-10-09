f471cdee492667450c2a7310fe11512bedde5b6c
rshatz <r_shatz@icloud.com>
2015-10-09 10:46:35 -0700

    Revert "menu"
    This reverts commit 0bd3f1a90806d0e835b83ceebe34b18b1bb6fb7d.

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -14,6 +14,26 @@ MainWindow::MainWindow()
 
 void MainWindow::createActions()
 {
+    newAction = new QAction(QIcon(":/images/New.png"), "New", this);
+    newAction->setShortcut(QKeySequence::New);
+    connect(newAction, SIGNAL(triggered(bool)),
+            this, SLOT(newFile()));
+
+    openAction = new QAction(QIcon(":/images/Open.png"), "Open", this);
+    openAction->setShortcut(QKeySequence::Open);
+    connect(openAction, SIGNAL(triggered(bool)),
+            this, SLOT(open()));
+
+    saveAction = new QAction(QIcon(":/images/Save.png"), "Save", this);
+    saveAction->setShortcut(QKeySequence::Save);
+    connect(saveAction, SIGNAL(triggered(bool)),
+            this, SLOT(save()));
+
+    saveAsAction = new QAction("Save As", this);
+    saveAsAction->setShortcut(QKeySequence::SaveAs);
+    connect(saveAsAction, SIGNAL(triggered(bool)),
+            this, SLOT(saveAs()));
+
     exitAction = new QAction("Exit", this);
     exitAction->setShortcut(tr("Ctrl+Q"));
     connect(exitAction, SIGNAL(triggered(bool)),
@@ -21,8 +41,33 @@ void MainWindow::createActions()
 }
 
 void MainWindow::createMenus()
-
 {
     fileMenu = menuBar()->addMenu("File");
+    fileMenu->addAction(newAction);
+    fileMenu->addAction(openAction);
+    fileMenu->addAction(saveAction);
+    fileMenu->addAction(saveAsAction);
+    fileMenu->addSeparator();
     fileMenu->addAction(exitAction);
+
+}
+
+void MainWindow::newFile()
+{
+
+}
+
+void MainWindow::open()
+{
+
+}
+
+void MainWindow::save()
+{
+
+}
+
+void MainWindow::saveAs()
+{
+
 }
